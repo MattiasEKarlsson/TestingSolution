9f653bddff164114a922d021601477ded2e50e2e
Mattias Karlsson <Mattias.E.Karlsson@outlook.com>
2020-09-23 07:34:58 +0200

    Add project files.

---
+++ SharedUwpLibrary/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedUwpLibrary.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        public Employee()
+        {
+        }
+
+        public Employee(string firstname, string lastname)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        public Employee(string firstname, string lastname, string department)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstname;
+            LastName = lastname;
+            Department = department;
+        }
+    }
+
+    
+   
+}
